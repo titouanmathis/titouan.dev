1332d631a3bf0d3f3efe0ff371c3e5942f8dba4a
Titouan Mathis <titouan@studiometa.fr>
2021-12-30 20:07:16 +0100

    Enable turbo mode

--- iles.config.ts
+++ iles.config.ts
@@ -3,4 +3,6 @@ import { defineConfig } from 'iles';
 export default defineConfig({
   modules: ['@islands/prism', '@islands/icons', '@islands/headings'],
   prettyUrls: true,
+  turbo: true,
+  siteUrl: process?.env?.DEPLOY_PRIME_URL,
 });
