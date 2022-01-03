2011fa4b3f919aef8aabe193582b45c2cbd111ef
Titouan Mathis <titouan@studiometa.fr>
2022-01-03 11:28:38 +0100

    Update iles with the `ERR_REQUIRE_ESM` fix when building

--- iles.config.ts
+++ iles.config.ts
@@ -1,17 +1,5 @@
-import { promises as fs } from 'fs';
-import { join } from 'path';
-import { defineConfig, resolveConfig } from 'iles';
+import { defineConfig } from 'iles';
 import remarkGfm from 'remark-gfm';
-
-function fixSsrRequireEsmError() {
-  return process.env.NODE_ENV === 'production' && {
-    name: 'fix-ssr-require-esm-error',
-    async writeBundle() {
-      const config = await resolveConfig();
-      fs.writeFile(join(config.tempDir, 'package.json'), JSON.stringify({ type: 'commonjs' }));
-    },
-  };
-}
 
 export default defineConfig({
   modules: ['@islands/prism', '@islands/icons', '@islands/headings'],
@@ -20,8 +8,5 @@ export default defineConfig({
   siteUrl: process?.env?.DEPLOY_PRIME_URL,
   markdown: {
     remarkPlugins: [remarkGfm],
-  },
-  vite: {
-    plugins: [fixSsrRequireEsmError()],
   },
 });
