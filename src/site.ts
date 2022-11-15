b3ee733a2c3c3f8dea3f6e3a16516134771b9d87
Titouan Mathis <titouan@studiometa.fr>
2022-11-15 21:22:33 +0100

    Update title and description

--- src/site.ts
+++ src/site.ts
@@ -1,6 +1,6 @@
 export default {
-  title: 'Titouan Mathis — CTO at Studio Meta',
+  title: 'Titouan Mathis — Developer and CTO at Studio Meta',
   author: 'Titouan Mathis',
   description:
-    'Notes and experiments by Titouan Mathis, CTO at Studio Meta between Paris and Strasbourg, France.',
+    'Notes and experiments by Titouan Mathis, developer and CTO at Studio Meta between Paris and Strasbourg, France.',
 };
