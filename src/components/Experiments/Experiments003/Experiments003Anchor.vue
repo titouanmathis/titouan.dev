10dc1546fe0c968cce2bb991da2665da0313458d
Titouan Mathis <titouan@studiometa.fr>
2022-11-28 17:53:35 +0100

    Add appear transition

--- src/components/Experiments/Experiments003/Experiments003Anchor.vue
+++ src/components/Experiments/Experiments003/Experiments003Anchor.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-  import { ref, unref, computed, watch } from 'vue';
-  import { useRafFn, usePointer, useWindowSize, useThrottledRefHistory } from '@vueuse/core';
-  import { map, damp } from '@studiometa/js-toolkit/utils';
+  import { ref, unref, computed, watch, onMounted } from 'vue';
+  import { useRafFn, usePointer, useWindowSize, useEventListener } from '@vueuse/core';
+  import { map, damp, tween, easeOutExpo } from '@studiometa/js-toolkit/utils';
   import { degToRad } from '~/utils/math.js';
 
   const props = defineProps({
@@ -22,19 +22,28 @@
     }, (props.index / props.total) * 1000);
   });
 
-  const progressX = ref(0.5);
-  const progressY = ref(0.5);
+  const progress = ref(0);
+  const progressX = ref(0.75);
+  const progressY = ref(0.75);
   const angle = computed(() =>
     degToRad((360 / (props.total - props.delta - 1)) * (props.index - props.delta / 2))
   );
   const x = computed(() => (props.size / 2) * Math.cos(unref(angle)));
   const y = computed(() => (props.size / 2) * Math.sin(unref(angle)));
   const z = computed(() => props.index * progressY.value * -0.5)
-  const scale = computed(() => map(progressX.value, 0, 1, 0.75, 1.75));
+  const scale = computed(() => map(progressX.value, 0, 1, 0.5, 1));
 
   const styles = computed(() => ({
     zIndex: props.total - props.index,
-    transform: `translate3d(${x.value}px, ${y.value}px, ${z.value}px) scaleX(${scale.value}) scaleY(${scale.value})`,
+    transform: `
+      translate3d(
+        ${x.value * progress.value}px,
+        ${y.value * progress.value}px,
+        ${z.value}px
+      )
+      scaleX(${scale.value * progress.value})
+      scaleY(${scale.value * progress.value})
+    `,
   }));
 
   const innerStyles = computed(() => ({
@@ -42,21 +51,43 @@
     animationDuration: `${props.duration}s`,
   }));
 
-  useRafFn(() => {
-    progressX.value = damp(delayedPointerX.value / width.value, progressX.value, 0.1, 0.01);
-    progressY.value = damp(pointerY.value / height.value, progressY.value, 0.1, 0.01);
+  onMounted(() => {
+    useEventListener(document, 'mousemove', () => {
+      useRafFn(() => {
+        progressX.value = damp(delayedPointerX.value / width.value, progressX.value, 0.1, 0.01);
+        progressY.value = damp(pointerY.value / height.value, progressY.value, 0.1, 0.01);
+      });
+    }, { once: true });
   });
+
+  /**
+   * Enter.
+   */
+  function enter() {
+    return new Promise((resolve) => {
+      const t = tween((p) => {
+        progress.value = p;
+      }, {
+        easing: easeOutExpo,
+        duration: 1,
+        onFinish: resolve,
+      });
+      setTimeout(() => t.start(), props.index * 20);
+    })
+  }
 </script>
 
 <template>
-  <div :style="styles" class="absolute top-1/2 left-1/2 w-[2px] h-[2px] -mt-px -ml-px anchor">
-    <div :style="innerStyles" class="absolute top-1/2 left-1/2 bg-secondary border border-primary anchor__inner" />
-  </div>
+  <transition appear @enter="enter">
+    <div :style="styles" class="absolute top-1/2 left-1/2 w-[2px] h-[2px] -mt-px -ml-px anchor">
+      <div :style="innerStyles" class="absolute top-1/2 left-1/2 bg-secondary border border-primary anchor__inner" />
+    </div>
+  </transition>
 </template>
 
 <style scoped>
   .anchor__inner {
-    --size: 5vmax;
+    --size: 10vmax;
     width: var(--size);
     height: calc(var(--size) * 2);
     margin-top: calc(var(--size) * -1);
