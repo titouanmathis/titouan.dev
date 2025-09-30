7d8d1b4641e49d76110bfdef175ee129efa7f4b9
Titouan Mathis <titouan@studiometa.fr>
2025-09-30 20:25:17 +0200

    Improve transition

--- src/components/ButtonToggleTheme.vue
+++ src/components/ButtonToggleTheme.vue
@@ -3,22 +3,17 @@
 
   const isDark = useDark();
   const toggleDark = useToggle(isDark);
-
-  function onClick() {
-    console.log('toggle dark');
-    toggleDark();
-  }
 </script>
 
 <template>
   <button
-    @click="onClick"
+    @click="() => toggleDark()"
     class="w-5 h-5"
     type="button"
     aria-label="Toggle theme">
     <Transition
-      enter-from-class="opacity-0 translate-y-full"
-      leave-to-class="opacity-0 -translate-y-full">
+      enter-from-class="opacity-0 scale-50 -translate-y-full"
+      leave-to-class="opacity-0 scale-50 translate-y-full">
       <IconBxBxMoon
         v-if="!isDark"
         class="absolute top-0 left-0 block transition duration-500 ease-out-expo" />
